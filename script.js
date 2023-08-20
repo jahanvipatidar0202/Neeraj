52f338921018b571f1798acb1cf6e14d02057797
Jahanvi Patidar <jahanvipatidar20@gmail.com>
2023-08-21 00:17:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,245 @@
+var mySong1 = new Audio();
+var mySong2 = new Audio();
+var mySong3 = new Audio();
+var mySong4 = new Audio();
+var mySong5 = new Audio();
+var mySong6 = new Audio();
+var mySong7 = new Audio();
+var mySong8 = new Audio();
+var mySong9 = new Audio();
+var mySong10 = new Audio();
+var mySong11 = new Audio();
+var mySong12 = new Audio();
+var mySong13 = new Audio();
+var mySong14 = new Audio();
+var mySong15 = new Audio();
+var mySong16 = new Audio();
+var mySong17 = new Audio();
+var mySong18 = new Audio();
+var mySong19 = new Audio();
+var mySong20 = new Audio();
+var mySong21 = new Audio();
+var mySong22 = new Audio();
+var mySong23 = new Audio();
+var mySong24 = new Audio();
+var mySong25 = new Audio();
+var mySong26 = new Audio();
+var mySong27 = new Audio();
+mySong1.src = "../Aaj Sajeya - Goldie Sohel 128 Kbps.mp3";
+mySong2.src = "../Chedkhaniyaan_64(MyMp3Song).mp3";
+mySong3.src = "../Abhi Kuch Dino Se.mp3";
+mySong4.src = "../Taarif Karoon (Sanam Puri).mp3";
+mySong5.src = "../Aise-Kyun(PaglaSongs) (1).mp3";
+mySong6.src = "../Samjhawan_192(Ghantalele.com).mp3";
+mySong7.src =
+  "../Mere Liye Tum Kaafi Ho Shubh Mangal Zyada Saavdhan 128 Kbps.mp3";
+mySong8.src = "../Jaise Mera Tu 128 Kbps.mp3";
+mySong9.src = "../Sang-Rahiyo---Ranveer-Allahbadia(pagalworld.co.uk).mp3";
+mySong10.src = "../Reply Of Chand Baliyan Swati Mishra.mp3";
+mySong11.src = "../Tera Hua Loveyatri A Journey Of Love 128 Kbps.mp3";
+mySong12.src = "../old_OUATIM-Tum Jo Aaye.mp3";
+mySong13.src = "../Jaadui_320(PagalWorld.com.se).mp3";
+mySong14.src = "../old_CKNG-Haal Kaisa Hai Janab Ka.mp3";
+mySong15.src = "../Nazm-Nazm-feat.-Ayushmann-Khurrana-Ayushmann-Khurrana.mp3";
+mySong16.src = "../Socha Hai (Baadshaho) 128 Kbps.mp3";
+mySong17.src =
+  "../Chann Vi Gawah - Madhav Mahajan, Himani Kapoor, Angela Krislinzki 128 Kbps.mp3";
+mySong18.src =
+  "../Tera Hone Laga Hoon - Ajab Prem Ki Ghazab Kahani 128 Kbps.mp3";
+mySong19.src = "../Phir Aur Kya Chahiye_64(PagalWorld.com.pe).mp3";
+mySong20.src = "../Gazab Ka Hai Din (Dil Juunglee) 128 Kbps.mp3";
+mySong21.src = "../new_128_Tu Mileya - Darshan Raval.mp3";
+mySong22.src = "../new_128_03 - Dil Jaaniye - Khandaani Shafakhana (2019).mp3";
+mySong23.src = "../Atak Gaya - Acoustic_192(Ghantalele.com).mp3";
+mySong24.src = "../Mileya Mileya 128 Kbps.mp3";
+mySong25.src = "../Mahiya-Jina-Sohna(PaglaSongs).mp3";
+mySong26.src = "../Afeemi (Meri Pyaari Bindu) 128 Kbps.mp3";
+mySong27.src = "../Main-Rang-Sharbaton-Ka(PaglaSongs).mp3";
+const playSong1 = () => {
+  mySong1.play();
+};
+const pauseSong1 = () => {
+  mySong1.pause();
+};
+const playSong2 = () => {
+  mySong2.play();
+};
+
+const pauseSong2 = () => {
+  mySong2.pause();
+};
+const playSong3 = () => {
+  mySong3.play();
+};
+
+const pauseSong3 = () => {
+  mySong3.pause();
+};
+const playSong4 = () => {
+  mySong4.play();
+};
+
+const pauseSong4 = () => {
+  mySong4.pause();
+};
+const playSong5 = () => {
+  mySong5.play();
+};
+
+const pauseSong5 = () => {
+  mySong5.pause();
+};
+const playSong6 = () => {
+  mySong6.play();
+};
+
+const pauseSong6 = () => {
+  mySong6.pause();
+};
+const playSong7 = () => {
+  mySong7.play();
+};
+
+const pauseSong7 = () => {
+  mySong7.pause();
+};
+const playSong8 = () => {
+  mySong8.play();
+};
+
+const pauseSong8 = () => {
+  mySong8.pause();
+};
+const playSong9 = () => {
+  mySong9.play();
+};
+
+const pauseSong9 = () => {
+  mySong9.pause();
+};
+const playSong10 = () => {
+  mySong10.play();
+};
+
+const pauseSong10 = () => {
+  mySong10.pause();
+};
+const playSong11 = () => {
+  mySong11.play();
+};
+
+const pauseSong11 = () => {
+  mySong11.pause();
+};
+const playSong12 = () => {
+  mySong12.play();
+};
+
+const pauseSong12 = () => {
+  mySong12.pause();
+};
+const playSong13 = () => {
+  mySong13.play();
+};
+
+const pauseSong13 = () => {
+  mySong13.pause();
+};
+const playSong14 = () => {
+  mySong14.play();
+};
+
+const pauseSong14 = () => {
+  mySong14.pause();
+};
+const playSong15 = () => {
+  mySong15.play();
+};
+
+const pauseSong15 = () => {
+  mySong15.pause();
+};
+const playSong16 = () => {
+  mySong16.play();
+};
+
+const pauseSong16 = () => {
+  mySong16.pause();
+};
+const playSong17 = () => {
+  mySong17.play();
+};
+
+const pauseSong17 = () => {
+  mySong17.pause();
+};
+const playSong18 = () => {
+  mySong18.play();
+};
+
+const pauseSong18 = () => {
+  mySong18.pause();
+};
+const playSong19 = () => {
+  mySong19.play();
+};
+
+const pauseSong19 = () => {
+  mySong19.pause();
+};
+const playSong20 = () => {
+  mySong20.play();
+};
+
+const pauseSong20 = () => {
+  mySong20.pause();
+};
+const playSong21 = () => {
+  mySong21.play();
+};
+
+const pauseSong21 = () => {
+  mySong21.pause();
+};
+const playSong22 = () => {
+  mySong22.play();
+};
+
+const pauseSong22 = () => {
+  mySong22.pause();
+};
+const playSong23 = () => {
+  mySong23.play();
+};
+
+const pauseSong23 = () => {
+  mySong23.pause();
+};
+const playSong24 = () => {
+  mySong24.play();
+};
+
+const pauseSong24 = () => {
+  mySong24.pause();
+};
+const playSong25 = () => {
+  mySong25.play();
+};
+
+const pauseSong25 = () => {
+  mySong25.pause();
+};
+const playSong26 = () => {
+  mySong26.play();
+};
+7;
+const pauseSong26 = () => {
+  mySong26.pause();
+};
+const playSong27 = () => {
+  mySong27.play();
+};
+
+const pauseSong27 = () => {
+  mySong27.pause();
+};
